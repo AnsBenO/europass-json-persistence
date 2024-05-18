cbbd953d1f4c10c2532fd1aa371c44cfd90c5515
AnsBenO <ans.benomar@gmail.com>
2024-05-18 15:08:26 +0100

    empty push

--- src/main/java/com/nttdata/App.java
+++ src/main/java/com/nttdata/App.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.nttdata.repositories.LanguageRepository;
 import com.nttdata.utils.JSON;
@@ -13,6 +12,7 @@ import org.slf4j.LoggerFactory;
 
 public class App {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
+
     public static void main(String[] args) throws IOException {
         final LanguageRepository languageRepository = new LanguageRepository();
 
